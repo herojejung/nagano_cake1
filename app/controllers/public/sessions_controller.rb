072a210945eb89cebe21c77370148712678d2260
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2022-12-16 01:17:38 +0000

    [update]mypage

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,20 +2,8 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
-protected
-
-  def configure_permitted_parameters
-    added_attrs = [:last_name,:first_name,:last_name_kana,:first_name_kana,:email,:encrypted_password,:postal_code,:adresses,:telephone_number ]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-  end
-
   def after_sign_in_path_for(resource)
-    public_customers_path
-  end
-
-  def after_sign_up_path_for(resource)
-    public_customers_path(current_user.id)
+    public_root_path
   end
 
   def after_sign_out_path_for(resource)
