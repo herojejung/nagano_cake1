bc6467de94bfbc7537c24fc3cfa50423323f0889
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2022-12-25 01:30:33 +0000

    [add]serch_genre

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,6 +2,12 @@ class Public::ItemsController < ApplicationController
 
   def index
     @items = Item.all
+    @genres = Genre.all
+  if params[:genre_id].present?
+      #presentメソッドでparams[:category_id]に値が含まれているか確認 => trueの場合下記を実行
+    @genre = Genre.find(params[:genre_id])
+    @items = @genre.items
+  end
   end
 
   def show
