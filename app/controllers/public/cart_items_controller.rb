8d1289b53c4453facde2aba02de2bc82e862f8d0
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2022-12-27 10:09:32 +0000

    [update]cart_item_create

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,14 +3,14 @@ def create
   @cart_item = CartItem.new(cart_item_params)
   @cart_item.customer_id = current_customer.id
 if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-  current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).number += params[:amount].to_i
-  current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).save
+  @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+  @cart_item.amount += params[:cart_item][:item_id].to_i
+  @cart_item.save
   redirect_to cart_items_path
-if @cart_item.save
+elsif @cart_item.save
   redirect_to cart_items_path
 else
   render:new
-end
 end
 end
 
