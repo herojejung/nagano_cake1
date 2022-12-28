06c92c88cb1bc6136ef6390aefa097a1850d35bf
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2022-12-28 01:18:20 +0000

    [add]order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,12 +2,13 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
   end
-  
+
   def create
     @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
   if @order.save
     redirect_to confirm_path
-    
+  end
   end
 
   def confirm
@@ -21,11 +22,11 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
-  
+
   private
 
 def order_params
-  params.require(:order).permit(:customer_id,:delivery_postal_code,:delivery_adress,:delivery_name,:shipping_fee,:bill,:payment_method,:status)
+  params.require(:order).permit(:customer_id,:delivery_postal_code,:delivery_address,:delivery_name,:shipping_fee,:bill,:payment_method,:status)
 end
-  
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  belongs_to :customer
+  belongs_to :customers
   has_many :order_detailas
 
   enum payment_method: { credit_card: 0, transfer: 1 }
