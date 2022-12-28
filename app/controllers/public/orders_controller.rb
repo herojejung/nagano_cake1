f52692c74560ad2fcbe9abf2b74095a205f32bc8
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2022-12-28 08:38:26 +0000

    [update]orders_controller

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -14,10 +14,22 @@ class Public::OrdersController < ApplicationController
   def confirm
     @cart_item = current_customer.cart_items
     @order = Order.new(order_params)
+  if params [:order][:select_address] == "0"
     @order.delivery_address  = current_customer.address
-    @order.delivery_name  = current_customer.last_name
+    @order.delivery_name  = current_customer.first_name+current_customer.last_name
     @order.delivery_postal_code  = current_customer.postal_code
-    byebug
+  elsif params [:order][:select_address] == "1"
+    @address = Address.find(params[:order][:address_id])
+    @order.delivery_address  = address.address
+    @order.delivery_name  = address.first_name+current_customer.last_name
+    @order.delivery_postal_code  = address.postal_code
+  elsif params [:order][:select_address] == "2"
+    @order.delivery_address  = params[:order][:delivery_address]
+    @order.delivery_name  = params[:order][:delivery_name]
+    @order.delivery_postal_code  = params[:order][:delivery_postal_code]
+  else
+    render :new
+  end
   end
 
   def complete
