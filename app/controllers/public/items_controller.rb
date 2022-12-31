79feb22daa1202f8635ccb1ad280bd1ef46d98f3
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2022-12-31 01:42:52 +0000

    [update]item_show

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -13,6 +13,12 @@ class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem
+        @genres = Genre.all
+  if params[:genre_id].present?
+      #presentメソッドでparams[:category_id]に値が含まれているか確認 => trueの場合下記を実行
+    @genre = Genre.find(params[:genre_id])
+    @items = @genre.items
+  end
   end
 
 private
