c10692168bd5968ce8407b92e9e4f52b3c29f56a
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-01-04 07:01:39 +0000

    [update]order_confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,7 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
+    @customer = current_customer
   end
 
   def create
@@ -14,16 +15,18 @@ class Public::OrdersController < ApplicationController
   def confirm
     @cart_item = current_customer.cart_items
     @order = Order.new(order_params)
-  if params [:order][:select_address] == "0"
+    @order.customer_id = current_customer.id
+    
+  if params[:order][:select_address] == "0"
     @order.address  = current_customer.address
     @order.name  = current_customer.first_name+current_customer.last_name
     @order.postal_code  = current_customer.postal_code
-  elsif params [:order][:select_address] == "1"
+  elsif params[:order][:select_address] == "1"
     @address = Address.find(params[:order][:address_id])
-    @order.address  = address.address
-    @order.name  = address.first_name+current_customer.last_name
-    @order.postal_code  = address.postal_code
-  elsif params [:order][:select_address] == "2"
+    @order.address  = @address.address
+    @order.name  = @address.first_name+current_customer.last_name
+    @order.postal_code  = @address.postal_code
+  elsif params[:order][:select_address] == "2"
     @order.address  = params[:order][:address]
     @order.name  = params[:order][:name]
     @order.postal_code  = params[:order][:postal_code]
@@ -44,7 +47,7 @@ class Public::OrdersController < ApplicationController
   private
 
 def order_params
-  params.require(:order).permit(:customer_id,:postal_code,:address,:name,:shipping_fee,:bill,:payment_method,:status,:select_address)
+  params.require(:order).permit(:customer_id,:postal_code,:address,:name,:shipping_fee,:bill,:payment_method,:status)
 end
 
 end
