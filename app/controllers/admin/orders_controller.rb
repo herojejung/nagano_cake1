0c93ceacdd9fd1e8c61f6929268c44e6f0396ecb
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-01-08 09:01:51 +0000

    [update]admin_order_order_details

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,22 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetails.all
   end
+
+  def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: params[:id])
+  if @order.update(order_params)
+    @order_details.update_all(making_status: 1) if @order.status == "payment_confirmation"
+    ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
+    redirect_to admin_order_path
+  end
+  end
+
+private
+
+def order_params
+  params.require(:order).permit(:customer_id,:postal_code,:address,:name,:shipping_fee,:bill,:payment_method,:status)
+end
 end
